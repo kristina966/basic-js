2d53715852df4d0565ae7cbf9f95954cc2097e93
kristina966 <kristina.drozdova96@gmail.com>
2022-05-01 16:02:58 +0300

    feat: delete task

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,10 +1,1 @@
 
-def matrixElementsSum(matrix):
-    costSum = 0
-    for x in range(len(matrix)):
-        for y in range(len(matrix[0])):
-            if x>0:
-                if matrix[x-1][y] == 0:
-                    matrix[x][y] = 0
-            costSum += matrix[x][y]
-    return costSum
